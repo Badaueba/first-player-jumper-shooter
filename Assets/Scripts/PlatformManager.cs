842efb4f964bf8984dc986077997f7547506ac0b
Badaueba <alan.develop@outlook.com>
2016-05-11 15:52:21 -0300

    spawn e destroy das plataformas

--- Assets/Scripts/PlatformManager.cs
+++ Assets/Scripts/PlatformManager.cs
@@ -5,12 +5,12 @@ using System.Collections.Generic;
 public class PlatformManager : MonoBehaviour {
 	
 	public Transform reference;
-	public Transform[] plataforms;
+	private Transform[] plataforms;
 	Transform t;
 	void Start() {
+		plataforms = GameObject.FindObjectOfType<PlatformSpawner> ().platforms;
 		reference = Transform.FindObjectOfType<PlayerControl> ().transform;
 		t = this.GetComponent<Transform> ();
-
 		StartCoroutine (Change ());
 	}
 
@@ -18,7 +18,10 @@ public class PlatformManager : MonoBehaviour {
 		while (true) {
 			float zPosition = t.position.z + t.localScale.z/2;
 			if ( zPosition < reference.position.z)  {
-				transform.position = new Vector3 (0, 0, t.position.z + (4 *t.localScale.z));
+				Vector3 pos = new Vector3 (0, 0, t.position.z + (plataforms.Length * t.localScale.z));
+				int rand = Random.Range(0, plataforms.Length);
+				Instantiate(plataforms[rand], pos, Quaternion.identity);
+				Destroy(this.gameObject);
 			}
 			yield return new WaitForEndOfFrame();
 		}
