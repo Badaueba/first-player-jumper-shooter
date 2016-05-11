5f45417863cd9d87b2fd7503a3c76bd8b95bb59b
Badaueba <alan.develop@outlook.com>
2016-05-11 12:36:28 -0300

    first commit

---
+++ Assets/Scripts/PlayerCol.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerCol : MonoBehaviour {
+
+	
+	void OnTriggerEnter (Collider trig) {
+		if (trig.gameObject.tag == "Coin") {
+			Destroy(trig.gameObject);
+		}
+		if (trig.gameObject.tag == "Obstacle") {
+			StartCoroutine( PlayerDies(1f));
+		}
+		if (trig.gameObject.tag == "End") {
+			Debug.Log ("End");
+			Text finishText = GameObject.Find("Feedback").GetComponent<Text>();
+			finishText.text = "Finished!";
+			Image crosshair = GameObject.Find ("Crosshair").GetComponent<Image>();
+			crosshair.enabled = false;
+
+			StartCoroutine( PlayerDies(3f));
+		}
+	}
+
+	void OnCollisionEnter (Collision col) {
+		if (col.gameObject.tag == "Obstacle") {
+			StartCoroutine( PlayerDies(1f));
+		}
+	
+	}
+	
+	IEnumerator PlayerDies(float seconds) {
+		this.gameObject.GetComponent<PlayerControl> ().enabled = false;
+		this.gameObject.GetComponent<PlayerMove> ().enabled = false;
+		yield return new WaitForSeconds (seconds);
+		//Text finishText = GameObject.Find("Feedback").GetComponent<Text>();
+		//finishText.text = "Restarting!";
+		//activate UI for restarting
+		Application.LoadLevel (0);
+	}
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	public float jetPackFuel; 
+	public float jetPackForce;
+	public Rigidbody rb;
+
+	void Update () {
+		if (Input.GetButton ("Jump") || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) ) {
+			if (jetPackFuel > 0)
+				BoostUp ();
+		} 
+
+	}
+
+	void BoostUp () {
+		jetPackFuel = Mathf.MoveTowards (jetPackFuel, 0, Time.deltaTime);
+		rb.AddForce (new Vector3 (0, jetPackForce, 0));
+		rb.transform.Translate( new Vector3(0, Time.deltaTime * jetPackForce, 0));
+		Debug.Log ("rigdbody : " + rb);
+	}
+
+	void OnCollisionStay(Collision col) {
+		if (col.gameObject.tag == "Ground") {
+			if (jetPackFuel < 1.5f  ) {
+				jetPackFuel = Mathf.MoveTowards(jetPackFuel, 1.5f, Time.fixedDeltaTime);
+			}
+		}
+	}
+
+}
